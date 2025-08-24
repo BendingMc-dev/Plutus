0b959d11b61964ed969b8fd3f2bbc0590a8297ce
Ryan Tapfield <108175935+RyanDusty@users.noreply.github.com>
2025-08-25 09:16:45 +1000

    Update Database.java

--- src/main/java/me/ryandusty/plutus/db/Database.java
+++ src/main/java/me/ryandusty/plutus/db/Database.java
@@ -1,1 +1,26 @@
 
+public class Database {
+  private Plutus plutus;
+  private String path = "jbdc:sqlite:database.db";
+  
+  public Database(Plutus plutus) {
+    this.plutus = plutus;
+  }
+  public final static void createDB() {
+    try (var connection = DriverManager.getConnection(path)) {
+      if (connection != null) {
+        var meta = connection.getMetaData();
+          plutus.getLogger().info("Driver established:" + meta);
+          plutus.getLogger().info("A new database has been created.");
+      }
+    } catch (SQLException e) {
+      plutus.getLogger().info(e.getMessage());
+    }
+  }
+  public static void createTable() {
+    String table = "CREATE TABLE if not EXISTS items (" +
+                   "id INTEGER PRIMARY KEY," +
+                   "name text NOT NULL," +
+                   ")";
+  }
+}
