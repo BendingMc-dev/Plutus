edc65ff5b65b8328a5a451a68e2a01906587de36
Ryan Tapfield <108175935+RyanDusty@users.noreply.github.com>
2025-08-26 09:16:33 +1000

    Update Database.java

--- src/main/java/me/ryandusty/plutus/db/Database.java
+++ src/main/java/me/ryandusty/plutus/db/Database.java
@@ -2,12 +2,14 @@
 public class Database {
   private Plutus plutus;
   private String path = "jbdc:sqlite:database.db";
+  private DriverManager connection = DriverManager.getConnection(path);
   
   public Database(Plutus plutus) {
     this.plutus = plutus;
+    this.path = path;
   }
   public final static void createDB() {
-    try (var connection = DriverManager.getConnection(path)) {
+    try (connection) {
       if (connection != null) {
         var meta = connection.getMetaData();
           plutus.getLogger().info("Driver established:" + meta);
@@ -20,7 +22,20 @@ public class Database {
   public static void createTable() {
     String table = "CREATE TABLE if not EXISTS items (" +
                    "id INTEGER PRIMARY KEY," +
-                   "name text NOT NULL," +
-                   ")";
+                   "material text NOT NULL," +
+                   "cost INTEGER NOT NULL"
+                   ")";   
+    try (connection);
+    var statement = connection.createStatement()) {
+      statement.execute(table);
+    } catch (Exception e) {
+      plutus.getLogger().info("Exception e, failed to create table." + e);
+    }
+  }
+  public static String getPath() {
+    return this.path;
+  }
+  public final static DriverManager getConnection() {
+    return this.connection;
   }
 }
